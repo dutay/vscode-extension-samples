9333b3879249190888510e36d983f9b3ad15d64c
Pine Wu <octref@gmail.com>
2019-06-24 20:59:16 -0700

    Just for testing

--- helloworld-test-sample/src/test/runTest.ts
+++ helloworld-test-sample/src/test/runTest.ts
@@ -4,6 +4,14 @@ import { runTests } from 'vscode-test';
 
 async function main() {
 	try {
+		/**
+		 * Download an archive build and unzip it
+		 * Then replace this with absolute path to the executable
+		 * - Win: Code - Insiders.exe
+		 * - macOS: Visual Studio Code - Insiders.app/Contents/MacOS/Electron
+		 * - Linux: VSCode-linux-x64/code-insiders
+		 */
+
 		const vscodeExecutablePath = '/Users/pine/Desktop/Visual Studio Code - Insiders.app/Contents/MacOS/Electron';
 
 		// The folder containing the Extension Manifest package.json
--- helloworld-test-sample/src/test/suite/index.ts
+++ helloworld-test-sample/src/test/suite/index.ts
@@ -2,31 +2,6 @@ import * as path from 'path';
 import * as Mocha from 'mocha';
 import * as glob from 'glob';
 
-export function run(testsRoot: string, cb: (error: any, failures?: number) => void): void {
-	// Create the mocha test
-	const mocha = new Mocha({
-		ui: 'tdd'
-	});
-	// Use any mocha API
-	mocha.useColors(true);
-
-	glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-		if (err) {
-			return cb(err);
-		}
-
-		// Add files to the test suite
-		files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
-
-		try {
-			// Run the mocha test
-			mocha.run(failures => cb(null, failures));
-		} catch (err) {
-			cb(err);
-		}
-	});
-}
-
 export function run(): Promise<void> {
 	// Create the mocha test
 	const mocha = new Mocha({
@@ -38,6 +13,8 @@ export function run(): Promise<void> {
 	const testsRoot = path.resolve(__dirname, '..')
 
 	return new Promise((c, e) => {
+		// return e(new Error('Straight failure'));
+
 		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
 			if (err) {
 				return e(err);
